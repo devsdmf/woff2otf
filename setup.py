0ac29b8f08191ff842af182fd2f59377c39fbe5d
devsdmf <devsdmf@gmail.com>
2017-04-11 18:02:36 -0300

    removed long description from module setup

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     # module name and version
